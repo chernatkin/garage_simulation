16e52e467a9153ff2310e52aaa3d6a79a2f3b27f
Sergey Chernatkin <tchers@rambler.ru>
2016-02-18 23:46:49 +0300

    add assembly configs

--- src/main/java/org/chernatkin/garage/simulation/service/GarageResource.java
+++ src/main/java/org/chernatkin/garage/simulation/service/GarageResource.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
